cc786260651d20abd21b5a5e10c00a61134bed96
David Kahara <kaharadave@gmail.com>
2019-08-18 19:42:26 +0300

    Consume firebase cloud endpoints

--- src/App.js
+++ src/App.js
@@ -1,26 +1,149 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {http} from './utilities/http'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      buyItems: [],
+      message: '',
+    }
+  }
+
+
+  componentDidMount() {
+
+    return http.get('/getItems').then((response) => {
+      this.setState({
+        buyItems: response.data
+      })
+    })
+  }
+
+  addItem(event) {
+    event.preventDefault()
+    const { buyItems } = this.state;
+    const newItem = this.newItem.value;
+
+    const isOnTheList = buyItems.includes(newItem)
+
+    if (isOnTheList) {
+      this.setState({
+        message: 'This item is already on the list'
+      })
+
+    } else {
+      return newItem !== '' && http.post('/addItem', { item: newItem }).then((response) => {
+        this.setState({
+          buyItems: response.data,
+          message: ''
+        })
+        this.addForm.reset()
+      })
+    }
+
+  }
+
+  removeItem(item){
+    const newBuyItems = this.state.buyItems.filter(buyItems => {
+      return item !== buyItems
+    })
+
+    return http.delete(`/deleteItem?id=${item.id}`).then((response) => {
+      this.setState({
+        buyItems: response.data
+      })
+    })
+
+    if(newBuyItems.length === 0){
+      this.setState({
+        message: 'No Item on the list, add some'
+      })
+    }
+  }
+
+  clearAll(){
+    this.setState({
+      buyItems: [],
+      message: 'No Item on the list, add some'
+    })
+  }
+
+  renderItems() {
+    let id = 1;
+    const { buyItems, message } = this.state;
+
+    return (
+      buyItems.length > 0 &&
+      <table className="table">
+        <caption>Shopping List</caption>
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Item</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            buyItems.map(item => {
+              console.log(item)
+              return (
+                <tr key={item.id}>
+                  <th scope="row">{id++}</th>
+                  <td>{item.items}</td>
+                  <td>
+                    <button onClick={(e) => this.removeItem(item)}  type="button" className="btn btn-default btn-sm">
+                      Remove
+                    </button>
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan="2">&nbsp;</td>
+            <td>
+              <button onClick={(e) => this.clearAll()}
+              className="btn btn-default btn-sm">Clear List</button>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    )
+  }
+
+
+
+  render() {
+    const { buyItems, message } = this.state;
+    return (
+      <div className="container">
+        <h1>Shopping List</h1>
+        <div className="content">
+
+          <form ref={input => {this.addForm = input}} className="form-inline" onSubmit={this.addItem.bind(this)}>
+            <div className="form-group">
+              <label htmlFor="newItemInput" className="sr-only">Add New Item</label>
+              <input ref={input => {this.newItem = input}}
+                type="text" className="form-control" id="newItemInput" />
+            </div>
+            <button className="btn btn-primary">Add</button>
+          </form>
+          {
+            (message !== '' || buyItems.length === 0) && <p className="message text-danger">{message}</p>
+          }
+
+         {this.renderItems()}
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
